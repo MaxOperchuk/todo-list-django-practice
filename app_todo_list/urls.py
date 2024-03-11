b0f3e43f7ecb8028b9b3d6b84aafe70d9e896435
Maksym Operchuk <operlast529@gmail.com>
2024-03-11 13:54:30 +0200

    add needed urls to app_todo_list/urls.py

--- app_todo_list/urls.py
+++ app_todo_list/urls.py
@@ -1,9 +1,19 @@
 from django.urls import path
 
-from .views import index
+from .views import (
+    IndexListView,
+    TaskCreateView,
+    TaskUpdateView,
+    TaskDeleteView,
+    complete_task_view
+)
 
 urlpatterns = [
-    path("", index, name="index"),
+    path("", IndexListView.as_view(), name="index"),
+    path("task/create/", TaskCreateView.as_view(), name="create-task"),
+    path("task/<int:pk>/update/", TaskUpdateView.as_view(), name="update-task"),
+    path("task/<int:pk>/delete/", TaskDeleteView.as_view(), name="delete-task"),
+    path("task/<int:pk>/complete/", complete_task_view, name="complete-task"),
 ]
 
 
